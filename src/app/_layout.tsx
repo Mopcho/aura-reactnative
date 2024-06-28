9f6e5356b64aaf5b071543294c03f4aa18a81b26
MopchoNex <v.petkov@softavail.com>
2024-06-28 03:53:15 +0300

    Setup project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function RootLayout() {
+  return (
+    <SafeAreaView>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </SafeAreaView>
+  );
+}
