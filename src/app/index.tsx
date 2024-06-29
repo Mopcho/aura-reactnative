8c18abecbd91477c2f41264c8cdf75f97fa35955
MopchoNex <v.petkov@softavail.com>
2024-06-29 16:21:17 +0300

    Created initial structure of different routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="text-3xl font-pblack">Aora</Text>
       <StatusBar style="auto" />
       <Link href="/profile" className="text-blue-400">
         Go to Profile
